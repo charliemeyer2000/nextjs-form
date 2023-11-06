2808ed09e6a83d1b0b02fab06d7fc28d9f9736ae
Charlie Meyer <charlie.g.meyer@icloud.com>
2023-11-05 20:54:37 -0500

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>MoniFlow Form</title>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✍️</text></svg>"
-        />
+        <link rel="icon" href="/moniflow.png" />
+        {/* open graph */}
+        <meta property="og:title" content="MoniFlow Form" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/moniflow.png" />
+        <meta property="og:description" content="MoniFlow Form" />
       </Head>
       <body>
         <Main />
